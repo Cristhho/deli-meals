3ea9b4523c77dbf5f8430a0211260e5b1c060ad7
cristhian ochoa <cristhho@gmail.com>
2019-11-04 19:06:07 -0500

    loading categories screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'category_item.dart';
+import 'dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deli Meals'),
       ),
-      children: <Widget>[
-
-      ],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: DUMMY_CATEGORIES.map((category) => CategoryItem(
+          category.title,
+          category.color
+        )).toList(),
+      ),
     );
   }
 }
