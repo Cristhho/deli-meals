9a3b1323c2523249789ad89785917d22b8dc1cfb
cristhian ochoa <cristhho@gmail.com>
2019-12-03 23:45:06 -0500

    Getting the recipes of a category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
 //  final String categoryId;
@@ -12,11 +14,16 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(child: Text("The recipes for the category"))
+      body: ListView.builder(itemBuilder: (ctx, index){
+        return Text(categoryMeals[index].title);
+      }, itemCount: categoryMeals.length,)
     );
   }
 }
